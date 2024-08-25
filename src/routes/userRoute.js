2190689ce53e09ea1d2499f773c4760f228b2e80
Bilal Farooq <117806307+bilal-the-dev@users.noreply.github.com>
2024-08-25 09:42:02 +0500

    change me to @me in user router

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,7 +6,7 @@ const {
 
 const router = express.Router();
 
-router.get("/me", getCurrentUser);
-router.get("/me/guilds", getCurrentUserGuilds);
+router.get("/@me", getCurrentUser);
+router.get("/@me/guilds", getCurrentUserGuilds);
 
 module.exports = router;
