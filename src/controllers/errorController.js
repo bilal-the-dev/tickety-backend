6c9bcddf57e6d831dbd0bb80d8604a2d912dfe3f
Bilal Farooq <117806307+bilal-the-dev@users.noreply.github.com>
2024-08-25 10:45:31 +0500

    fix error controller, production handling

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -8,7 +8,7 @@ const handleCastErrorDB = (err) => {
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 
-  const message = `Duplicate field value: ${value}. Please use another value!`;
+  const message = `Already in use: ${value}. Please use another!`;
   return new AppError(message, 400);
 };
 
@@ -16,6 +16,8 @@ const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
   const message = `Invalid input data. ${errors.join(". ")}`;
+  console.log(message);
+
   return new AppError(message, 400);
 };
 
@@ -27,7 +29,7 @@ const handleJWTExpiredError = () =>
 
 const sendErrorDev = (err, req, res) => {
   // A) API
-  res.status(err.statusCode).json({
+  return res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
@@ -37,39 +39,19 @@ const sendErrorDev = (err, req, res) => {
 
 const sendErrorProd = (err, req, res) => {
   // A) API
-  if (req.originalUrl.startsWith("/api")) {
-    // A) Operational, trusted error: send message to client
-    if (err.isOperational) {
-      return res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-      });
-    }
-    // B) Programming or other unknown error: don't leak error details
-    // 1) Log error
-    console.error("ERROR 💥", err);
-    // 2) Send generic message
-    return res.status(500).json({
-      status: "error",
-      message: "Something went very wrong!",
-    });
-  }
-
-  // B) RENDERED WEBSITE
   // A) Operational, trusted error: send message to client
-  if (err.isOperational) {
-    return res.status(err.statusCode).render("error", {
-      title: "Something went wrong!",
-      msg: err.message,
+  if (err.isOperational)
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
     });
-  }
   // B) Programming or other unknown error: don't leak error details
   // 1) Log error
-  console.error("ERROR 💥", err);
+  //   console.error("ERROR 💥", err);
   // 2) Send generic message
-  return res.status(err.statusCode).render("error", {
-    title: "Something went wrong!",
-    msg: "Please try again later.",
+  return res.status(500).json({
+    status: "error",
+    message: "Something went very wrong!",
   });
 };
 
@@ -82,14 +64,14 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
     error.message = err.message;
 
-    if (error.name === "CastError") error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === "ValidationError")
-      error = handleValidationErrorDB(error);
-    if (error.name === "JsonWebTokenError") error = handleJWTError();
-    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+    if (err.name === "CastError") error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (err.name === "ValidationError") error = handleValidationErrorDB(err);
+    if (err.name === "JsonWebTokenError") error = handleJWTError();
+    if (err.name === "TokenExpiredError") error = handleJWTExpiredError();
 
     sendErrorProd(error, req, res);
   }
