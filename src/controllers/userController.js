834f2af6ae718b93e2221894e8bae531b6419f1b
bilal-the-dev <bilalfarooq6001@gmail.com>
2024-10-18 10:23:59 +0500

    s

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,7 @@ exports.getCurrentUser = catchAsync(async (req, res, next) => {
 });
 
 exports.getCurrentUserGuilds = catchAsync(async (req, res, next) => {
-  const { adminOnly } = req.query;
+  const { adminOnly, botOnlyProp } = req.query;
   let guilds = await fetchUserGuildsOauth(req.dbUser.accessToken);
 
   const ADMIN_BIT = 0x0000000000000008;
@@ -15,7 +15,7 @@ exports.getCurrentUserGuilds = catchAsync(async (req, res, next) => {
   if (adminOnly == "true")
     guilds = guilds.filter((g) => (g.permissions & ADMIN_BIT) == ADMIN_BIT);
 
-  guilds = await addIsBotPresentProp(guilds);
+  if (botOnlyProp == "true") guilds = await addIsBotPresentProp(guilds);
 
   res.json({ status: "success", data: { user: req.discordUser, guilds } });
 });
